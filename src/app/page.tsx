37b75561067b30a513cea58bde287f01e3593522
Santiago Gomez <santigm1905@gmail.com>
2025-05-23 16:48:37 -0500

    main tags home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,28 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-      <main>
-      </main>
+      <div className="products">
+        <header className="header">
+          <nav className="navbar">
+            <Image src="./favicon.ico" alt="logo"></Image>
+            <div className="navbar__options">
+              <button className="navbar__user">
+                <Image src="./favicon.ico" alt="user icon"></Image>
+              </button>
+              <div className="navbar__line"></div>
+              <button className="navbar__cart">
+                <Image src="./favicon.ico" alt="shopping cart"></Image>
+              </button>
+            </div>
+          </nav>
+        </header>
+        <aside className="products__filters">
+          <h2>Categorias</h2>
+          <ul>
+            <li>Tech</li>
+          </ul>
+        </aside>
+        <main className="products__main"></main>
+      </div>
   );
 }
